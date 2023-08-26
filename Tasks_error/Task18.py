a6f1b0ef8a8dddef5dcae2feeb1e0a5914ca688d
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-08-25 20:12:04 -0500

    Update Task18.py

--- Tasks_error/Task18.py
+++ Tasks_error/Task18.py
@@ -1,11 +1,9 @@
 def add(a, b):
-    if a == b:
-        return a + b
-    else:
-        return a + b + 4
-
+        return a + b   # Modify inside of the function to print 10 and 25
+        
+# Don't touch lines below
 w = add(5,5)
-print(w)
+print(w)         # should print 10
 
 x = add(10,11)
-print(x)
+print(x)        # should print 25
